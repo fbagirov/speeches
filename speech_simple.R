7d6f97feded6b70f042e0e28efc051bb5ee667a8
fbagirov <fbagirov1@gmail.com>
2015-08-02 18:10:09 -0400

    Adding a simplified example modification

--- speech_simple.R
+++ speech_simple.R
@@ -25,17 +25,37 @@ docs <- tm_map(docs, stemDocument)
 
 
 #Create a Document Term Matrix
+dtm <- DocumentTermMatrix(docs)
+
 tdm <- TermDocumentMatrix(docs)
 
+
+
 #See the term frequencies
-freq <- colSums(as.matrix(tdm))
-ord <- order(freq)
-freq[head(ord)]
+freq_tdm <- colSums(as.matrix(tdm))
+ord_tdm <- order(freq_tdm)
+freq_tdm[head(ord_tdm)]
 
 #most frequent terms
-freq[tail(ord)]
+freq[tail(ord_tdm)]
 
 #frequency of frequencies
-head(table(freq), 15)
-tail(table(freq), 15)
+head(table(freq_tdm), 15)
+tail(table(freq_tdm), 15)
 
+#dtm
+#See the term frequencies
+freq_dtm <- colSums(as.matrix(dtm))
+ord_dtm <- order(freq_dtm)
+freq_dtm[head(ord_dtm)]
+
+#most frequent terms
+freq_dtm[tail(ord_dtm)]
+
+#frequency of frequencies
+head(table(freq_dtm), 15)
+tail(table(freq_dtm), 15)
+
+#Difference between TermDocumentMatrix and DocumentTermMatrix
+str(tdm)
+str(dtm)
